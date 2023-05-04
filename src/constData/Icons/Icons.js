b891c7af193f4143de8c0d5a679fcb81089033f3
nlprasanna5 <lakshmi.76740@gmail.com>
2023-05-04 15:21:08 +0530

    included all icons in a separate file

---
+++ src/constData/Icons/Icons.js
@@ -0,0 +1,137 @@
+
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
+import FormatBoldOutlinedIcon from '@mui/icons-material/FormatBoldOutlined';
+import FormatItalicOutlinedIcon from '@mui/icons-material/FormatItalicOutlined';
+import FormatUnderlinedOutlinedIcon from '@mui/icons-material/FormatUnderlinedOutlined';
+import ImagesearchRollerOutlinedIcon from '@mui/icons-material/ImagesearchRollerOutlined';
+import FormatColorTextOutlinedIcon from '@mui/icons-material/FormatColorTextOutlined';
+import BrushOutlinedIcon from '@mui/icons-material/BrushOutlined';
+import FormatIndentIncreaseIcon from '@mui/icons-material/FormatIndentIncrease';
+import LinkOutlinedIcon from '@mui/icons-material/LinkOutlined';
+import FormatAlignCenterOutlinedIcon from '@mui/icons-material/FormatAlignCenterOutlined';
+import FormatAlignLeftOutlinedIcon from '@mui/icons-material/FormatAlignLeftOutlined';
+import FormatAlignJustifyOutlinedIcon from '@mui/icons-material/FormatAlignJustifyOutlined';
+import FormatAlignRightOutlinedIcon from '@mui/icons-material/FormatAlignRightOutlined';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import FormatClearIcon from '@mui/icons-material/FormatClear';
+import FormatIndentDecreaseIcon from '@mui/icons-material/FormatIndentDecrease';
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+import SpellcheckOutlinedIcon from '@mui/icons-material/SpellcheckOutlined';
+import FormatLineSpacingIcon from '@mui/icons-material/FormatLineSpacing';
+import PrintOutlinedIcon from '@mui/icons-material/PrintOutlined';
+
+import ScreenWidth from "../../components/NavbarComponents/ScreenWidth/ScreenWidth";
+import FontStyle from "../../components/NavbarComponents/FontStyle/FontStyle";
+import FontSize from "../../components/NavbarComponents/FontSize/FontSize";
+
+
+export const Icons = [
+    {
+        icon: <UndoIcon />,
+        text: 'undo',
+    },
+    {
+        icon: <RedoIcon />,
+        text: 'redo',
+    },
+    {
+        icon: <PrintOutlinedIcon />,
+        text: 'print'
+
+    },
+    {
+        icon: <SpellcheckOutlinedIcon />,
+        text: 'spell check'
+    },
+    {
+        icon: <ImagesearchRollerOutlinedIcon />,
+        text: 'paint'
+    },
+    {
+        icon: <ScreenWidth />,
+        text: 'zoom'
+    },
+    {
+        icon: <FontStyle />,
+        text: 'font style'
+    },
+    {
+        icon: <FontSize />,
+        text: 'font size'
+    },
+
+    {
+        icon: <FormatBoldOutlinedIcon />,
+        text: 'bold',
+    },
+    {
+        icon: <FormatItalicOutlinedIcon />,
+        text: 'italic',
+    },
+    {
+        icon: <FormatUnderlinedOutlinedIcon />,
+        text: 'underline',
+    },
+
+    {
+        icon: <FormatColorTextOutlinedIcon />,
+        text: 'text color'
+    },
+    {
+        icon: <BrushOutlinedIcon />,
+        text: 'highlight color'
+    },
+    {
+        icon: <LinkOutlinedIcon />,
+        text: 'insert link'
+    },
+    {
+        icon: <InsertPhotoOutlinedIcon />,
+        text: 'insert image'
+    },
+    {
+        icon: <FormatAlignLeftOutlinedIcon />,
+        text: 'align left'
+    },
+    {
+        icon: <FormatAlignCenterOutlinedIcon />,
+        text: 'align center'
+    },
+    {
+        icon: <FormatAlignRightOutlinedIcon />,
+        text: 'align right'
+    },
+    {
+        icon: <FormatAlignJustifyOutlinedIcon />,
+        text: 'justify'
+    },
+    {
+        icon: <FormatLineSpacingIcon />,
+        text: 'line space'
+    },
+    {
+        icon: <FormatListBulletedIcon />,
+        text: 'bulleted list'
+    },
+    {
+        icon: <FormatListNumberedIcon />,
+        text: 'numbered list'
+    },
+    {
+        icon: <FormatIndentDecreaseIcon />,
+        text: 'decrease indent'
+    },
+    {
+        icon: <FormatIndentIncreaseIcon />,
+        text: 'increase indent'
+    },
+    
+    {
+        icon: <FormatClearIcon />,
+        text: 'clear format'
+    },
+
+
+]
