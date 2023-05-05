c71744707930297b5c1198d6b83eb6cc273c16ab
Neha-rajbhar <nr470245@gmail.com>
2023-05-06 00:56:17 +0530

    commit

--- src/components/NavbarComponents/selectImage/SelectImage.js
+++ src/components/NavbarComponents/selectImage/SelectImage.js
@@ -1,30 +1,42 @@
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
 import { RxImage } from "react-icons/rx";
 function SelectImage() {
-    let inputRef=useRef();
-    function handleImageOpen() {
-        inputRef.current.click();
-      }
-      function image(e) {
-        console.log(e.target.files[0]);
-        if (e.target.files[0]) {
-          document.execCommand(
-            "insertImage",
-            "",
-            URL.createObjectURL(e.target.files[0])
-          );
-        }
-      }
+  let inputRef = useRef();
+  function handleImageOpen() {
+    inputRef.current.click();
+  }
+  // function image(e) {
+  //   console.log(e.target.files[0]);
+  //   if (e.target.files[0]) {
+  //     document.execCommand(
+  //       "insertImage",
+  //       "",
+  //       URL.createObjectURL(e.target.files[0])
+  //     );
+  //   }
+  // }
+
+  function image(e) {
+    console.log(e.target.files[0]);
+    if (e.target.files[0]) {
+      let imgUrl = URL.createObjectURL(e.target.files[0]);
+      let img = document.createElement("img");
+      img.style.maxWidth = "100%"; // set the max-width to limit the size
+      img.style.maxHeight = "100%"; // set the max-height to limit the size
+      img.src = imgUrl;
+      document.execCommand("insertHTML", false, img.outerHTML);
+    }
+  }
   return (
-    <div>
+    <div style={{ maxWidth: "500px", maxHeight: "500px" }}>
       <button>
-            <label htmlFor="link">
-              <RxImage onClick={handleImageOpen} />
-            </label>
-          </button>
-          <input ref={inputRef} hidden onChange={image} type="file" />
+        <label htmlFor="link">
+          <RxImage onClick={handleImageOpen} />
+        </label>
+      </button>
+      <input ref={inputRef} hidden onChange={image} type="file" />
     </div>
-  )
+  );
 }
 
-export default SelectImage
+export default SelectImage;
